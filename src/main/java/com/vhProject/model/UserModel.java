8163288ca0c09d17f2d792993d67977210be396c
RMSapkale <rushikesh.s@integrationwings.com>
2024-11-04 19:58:00 +0530

    minor changes

--- src/main/java/com/vhProject/model/UserModel.java
+++ src/main/java/com/vhProject/model/UserModel.java
@@ -1,57 +1,22 @@
 package com.vhProject.model;
 
 
-import jakarta.persistence.*;
+import lombok.*;
+
+import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Table(name = "user")
 public class UserModel {
-    public UserModel() {
-    }
-
-    public UserModel(Long id, String name, String email, Long number) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.number = number;
-    }
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private Long number;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getNumber() {
-        return number;
-    }
-
-    public void setNumber(Long number) {
-        this.number = number;
-    }
 }
--- src/main/java/com/vhProject/repository/UserRepository.java
+++ src/main/java/com/vhProject/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.vhProject.repository;
 
 import com.vhProject.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
