616656c1a9a49c19a1102f906709e3f8b857ee8d
savim <savimsadhanameshram@gmail.com>
2024-11-21 11:48:06 +0530

    resend otp changes

--- src/main/java/com/vhProject/controller/UserController.java
+++ src/main/java/com/vhProject/controller/UserController.java
@@ -1,24 +1,13 @@
 package com.vhProject.controller;
 
-import com.vhProject.dto.RegisterDto;
 import com.vhProject.dto.LoginDto;
-import com.vhProject.model.UserModel;
+import com.vhProject.dto.RegisterDto;
 import com.vhProject.service.UserService;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestHeader;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
-
-import static com.vhProject.service.ResponseHandler.generateResponse;
 
 @RestController
 public class UserController {
@@ -35,10 +24,12 @@ public class UserController {
     public ResponseEntity<Object> verifyAccount(@RequestParam String email, @RequestParam Integer otp) {
         return userService.verifyAccount(email, otp);
     }
-    @PostMapping ("/regenerate-otp") //end point url
+
+    @PostMapping("/regenerate-otp") //end point url
     public ResponseEntity<Object> regenerateOtp(@RequestParam String email) throws MessagingException {
         return userService.regenerateOtp(email);
     }
+
     @PostMapping("/login")
     public ResponseEntity<Object> login(@RequestBody LoginDto loginDto) {
         return userService.login(loginDto);
@@ -48,16 +39,17 @@ public class UserController {
     public ResponseEntity<Object> forgotPassword(@RequestParam String email) throws MessagingException {
         return userService.forgotPassword(email);
     }
+
     @PutMapping("/set-password")
-    public ResponseEntity<Object> setPassword(@RequestParam String email, @RequestParam String  newPassword) throws MessagingException {
+    public ResponseEntity<Object> setPassword(@RequestParam String email, @RequestParam String newPassword) throws MessagingException {
         return userService.setPassword(email, newPassword);
     }
 
     @PutMapping("/resend-otp")
-        public ResponseEntity<Object> resendOtp(@RequestParam String email) throws MessagingException {
-            return userService.resendOtp(email);
-        }
+    public ResponseEntity<Object> resendOtp(@RequestParam String email) throws MessagingException {
+        return userService.resendOtp(email);
     }
+}
 
 
 
